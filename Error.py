0f44c108db12b93ca77782b7cc1b4ae4e3041953
Shivam Yadav <115492003+Shivamyadav4344@users.noreply.github.com>
2022-11-11 16:51:54 +0530

    Update Error.py

--- Error.py
+++ Error.py
@@ -5,13 +5,13 @@ class ValueLowError(Exception):
     pass
 
 def testValue(x):
-    if x > 100:
+    if x > 1000:
         raise ValueHighError('value is high')
     if x < 5:
         raise ValueLowError('value is low')
     
 try:
-    testValue(1000)
+    testValue(100000)
 except ValueHighError as e:
     print(e)
 except ValueLowError as e:
